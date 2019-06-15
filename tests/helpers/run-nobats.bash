8c2b277abfd02e9382bf93bbe28a7a1bb5865ffe
ctv10 <ctalledo@gmail.com>
2019-06-15 21:18:50 +0000

    Added docker_run() functions to sysvisor test helpers.

--- tests/helpers/run-nobats.bash
+++ tests/helpers/run-nobats.bash
@@ -6,6 +6,9 @@
 # These are useful for manually reproducing failures step-by-step by cut & pasting
 # test steps into the test container's shell.
 #
+# NOTE: Do *not* source this file from the bats tests; the tests
+# should source "run.bash" instead.
+#
 
 . $(dirname ${BASH_SOURCE[0]})/setup.bash
 
@@ -14,10 +17,20 @@ function sv_runc() {
   __sv_runc "$@"
 }
 
+function docker_run() {
+  docker run --runtime=sysvisor-runc --rm -d "$@"
+}
+
+function docker_stop() {
+  [[ "$#" == 1 ]]
+  docker stop -t 0 "$1"
+}
+
 function sv_mgr_start() {
   # Note: must match the way sysvisor-mgr is usually started, except
   # that we also pass in the "$@".
   sysvisor-mgr --log /dev/stdout "$@" > /var/log/sysvisor-mgr.log 2>&1
+  sleep 1
 }
 
 function sv_mgr_stop() {
--- tests/helpers/run.bash
+++ tests/helpers/run.bash
@@ -19,13 +19,32 @@ function sv_runc() {
 # Wrapper for docker using bats
 function docker() {
   run __docker "$@"
+
+  # Debug info (same as sv_runc())
   echo "docker $@ (status=$status):" >&2
-  echo "$output"
+  echo "$output" >&2
 }
 
 # Need this to avoid recursion on docker()
 function __docker() {
   command docker "$@"
+}
+
+# Executes docker run with sysvisor-runc; returns the container id
+function docker_run() {
+  docker run --runtime=sysvisor-runc --rm -d "$@"
+  [ "$status" -eq 0 ]
+
+  docker ps --format "{{.ID}}"
+  [ "$status" -eq 0 ]
+
+  echo "$output" | head -1
+}
+
+# Stops a docker container immediately
+function docker_stop() {
+  [[ "$#" == 1 ]]
+  docker stop -t 0 "$1"
 }
 
 # Run a background process under bats
@@ -39,6 +58,7 @@ function sv_mgr_start() {
   # Note: must match the way sysvisor-mgr is usually started, except
   # that we also pass in the "$@".
   bats_bg sysvisor-mgr --log /dev/stdout "$@" > /var/log/sysvisor-mgr.log 2>&1
+  sleep 1
 }
 
 function sv_mgr_stop() {
