d3136f7da4e860bcc8e1cb9ed2565cd8fbdb7f0f
ctalledo <ctalledo@nestybox.com>
2019-10-30 17:15:22 +0000

    Stabilize sys container with inner images build & commit tests.
    This change fixes a few bugs and makes some improvements
    to the sys container inner image build & commit tests.

--- tests/dind/docker-pull.sh
+++ tests/dind/docker-pull.sh
@@ -2,7 +2,6 @@
 
 # dockerd start
 dockerd > /var/log/dockerd.log 2>&1 &
-dockerd_pid=$!
 sleep 2
 
 # pull inner images
@@ -11,5 +10,7 @@ docker pull alpine:latest
 
 # dockerd cleanup (remove the .pid file as otherwise it prevents
 # dockerd from launching correctly inside sys container)
-kill $dockerd_pid
+kill $(cat /var/run/docker.pid)
+kill $(cat /run/docker/containerd/containerd.pid)
 rm -f /var/run/docker.pid
+rm -f /run/docker/containerd/containerd.pid
