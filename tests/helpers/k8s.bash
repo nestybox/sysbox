810484cc325b71a2606e94f15ad7e55cb906dddf
ctalledo <ctalledo@nestybox.com>
2020-02-28 20:22:09 -0800

    Added multi-cluster k8s test.

--- tests/helpers/k8s.bash
+++ tests/helpers/k8s.bash
@@ -20,17 +20,17 @@ function kubectl_config() {
 }
 
 function flannel_config() {
-  local master=$1
-  docker exec "$master" sh -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+  local k8s_master=$1
+  docker exec "$k8s_master" sh -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
   [ "$status" -eq 0 ]
 }
 
 function k8s_node_ready() {
-  local master=$1
+  local k8s_master=$1
   local node=$2
   local i
 
-  docker exec "$master" sh -c "kubectl get node $node"
+  docker exec "$k8s_master" sh -c "kubectl get node $node"
   [ "$status" -eq 0 ]
 
   echo ${lines[1]} | awk '{print $2}' | grep -qw "Ready"
@@ -71,11 +71,11 @@ function k8s_del_pod() {
 }
 
 function k8s_pod_ready() {
-  local master=$1
+  local k8s_master=$1
   local pod=$2
   local i
 
-  docker exec "$master" sh -c "kubectl get pod $pod"
+  docker exec "$k8s_master" sh -c "kubectl get pod $pod"
   [ "$status" -eq 0 ]
 
   local pod_status="${lines[1]}"
@@ -91,10 +91,10 @@ function k8s_pod_ready() {
 
 # Returns the IP address associated with a given pod
 function k8s_pod_ip() {
-  local master=$1
+  local k8s_master=$1
   local pod=$2
 
-  docker exec "$master" sh -c "kubectl get pod $pod -o wide"
+  docker exec "$k8s_master" sh -c "kubectl get pod $pod -o wide"
   [ "$status" -eq 0 ]
 
   local pod_status="${lines[1]}"
@@ -103,10 +103,10 @@ function k8s_pod_ip() {
 
 # Returns the node associated with a given pod
 function k8s_pod_node() {
-  local master=$1
+  local k8s_master=$1
   local pod=$2
 
-  docker exec "$master" sh -c "kubectl get pod $pod -o wide"
+  docker exec "$k8s_master" sh -c "kubectl get pod $pod -o wide"
   [ "$status" -eq 0 ]
 
   local pod_status="${lines[1]}"
@@ -115,11 +115,11 @@ function k8s_pod_node() {
 
 # Checks if a pod is scheduled on a given node
 function k8s_pod_in_node() {
-  local master=$1
+  local k8s_master=$1
   local pod=$2
   local node=$3
 
-  docker exec "$master" sh -c "kubectl get pod $pod -o wide"
+  docker exec "$k8s_master" sh -c "kubectl get pod $pod -o wide"
   [ "$status" -eq 0 ]
 
   local cur_node=$(echo "${lines[1]}" | awk '{print $7}')
@@ -129,11 +129,11 @@ function k8s_pod_in_node() {
 
 # Returns the IP address associated with a given service
 function k8s_svc_ip() {
-  local master=$1
+  local k8s_master=$1
   local ns=$2
   local svc=$3
 
- docker exec "$master" sh -c "kubectl --namespace $ns get svc $svc"
+ docker exec "$k8s_master" sh -c "kubectl --namespace $ns get svc $svc"
   [ "$status" -eq 0 ]
 
   local svc_status="${lines[1]}"
@@ -141,24 +141,24 @@ function k8s_svc_ip() {
 }
 
 function k8s_check_proxy_mode() {
-  local master=$1
+  local k8s_master=$1
   local proxy_mode=$2
 
-  docker exec "$master" sh -c "docker ps | grep kube-proxy | grep -v pause | awk '{print \$1}'"
+  docker exec "$k8s_master" sh -c "docker ps | grep kube-proxy | grep -v pause | awk '{print \$1}'"
   [ "$status" -eq 0 ]
 
   local kube_proxy=$output
 
-  docker exec "$master" sh -c "docker logs $kube_proxy 2>&1 | grep \"Using $proxy_mode Proxier\""
+  docker exec "$k8s_master" sh -c "docker logs $kube_proxy 2>&1 | grep \"Using $proxy_mode Proxier\""
   [ "$status" -eq 0 ]
 }
 
 function k8s_deployment_ready() {
-  local master=$1
+  local k8s_master=$1
   local ns=$2
   local deployment=$3
 
-  docker exec "$master" sh -c "kubectl --namespace $ns get deployment $deployment"
+  docker exec "$k8s_master" sh -c "kubectl --namespace $ns get deployment $deployment"
   [ "$status" -eq 0 ]
 
   local dpl_status="${lines[1]}"
@@ -173,22 +173,22 @@ function k8s_deployment_ready() {
 }
 
 function k8s_deployment_rollout_ready() {
-  local master=$1
+  local k8s_master=$1
   local ns=$2
   local deployment=$3
   local i
 
-  docker exec "$master" sh -c "kubectl --namespace $ns rollout status deployment.v1.apps/$deployment"
+  docker exec "$k8s_master" sh -c "kubectl --namespace $ns rollout status deployment.v1.apps/$deployment"
   [ "$status" -eq 0 ]
   [[ "$output" == "deployment \"$deployment\" successfully rolled out" ]]
 }
 
 function k8s_daemonset_ready() {
-  local master=$1
+  local k8s_master=$1
   local ns=$2
   local ds=$3
 
-  docker exec "$master" sh -c "kubectl --namespace $ns get ds $ds"
+  docker exec "$k8s_master" sh -c "kubectl --namespace $ns get ds $ds"
   [ "$status" -eq 0 ]
 
   local dpl_status="${lines[1]}"
@@ -212,25 +212,30 @@ function k8s_daemonset_ready() {
 # This function returns the "kubeadm join" string (in case the caller
 # wants to add more nodes need to be added to the cluster).
 #
-# usage: k8s_cluster_setup num_workers network
+# usage: k8s_cluster_setup cluster_name num_workers network
 #
+# cluster_name: name of the cluster; nodes in the cluster are named "<cluster_name>-master",
+#               "<cluster-name>-worker-0", "<cluster-name>-worker-1", etc.
 # num_workers: number of k8s worker nodes
 # network: docker network to which the k8s nodes are connected (e.g., bridge, user-defined, etc.)
 
 function k8s_cluster_setup() {
-  local num_workers=$1
-  local net=$2
+  local cluster_name=$1
+  local num_workers=$2
+  local net=$3
+
+  local k8s_master=${cluster_name}-master
   local pod_net_cidr=10.244.0.0/16
 
   #
-  # Deploy the master node (k8s-master)
+  # Deploy the master node
   #
 
-  docker_run --rm --network=$net --name=k8s-master --hostname=k8s-master nestybox/ubuntu-bionic-k8s:latest
+  docker_run --rm --network=$net --name=$k8s_master --hostname=$k8s_master nestybox/ubuntu-bionic-k8s:latest
 
-  wait_for_inner_dockerd k8s-master
+  wait_for_inner_dockerd $k8s_master
 
-  docker exec k8s-master sh -c "kubeadm init --kubernetes-version=v1.17.2 --pod-network-cidr=$pod_net_cidr"
+  docker exec $k8s_master sh -c "kubeadm init --kubernetes-version=v1.17.2 --pod-network-cidr=$pod_net_cidr"
   [ "$status" -eq 0 ]
   local kubeadm_output=$output
 
@@ -241,27 +246,29 @@ function k8s_cluster_setup() {
   [ "$status" -eq 0 ]
   local kubeadm_join=$output
 
-  kubectl_config k8s-master
+  kubectl_config $k8s_master
 
   # When the k8s cluster is on top of a docker user-defined network,
   # we need to modify the k8s coredns upstream forwarding to avoid DNS
   # loops (see sysbox issue #512).
   if [ "$net" != "bridge" ]; then
-    docker exec k8s-master kubectl -n kube-system patch configmap/coredns -p '{"data":{"Corefile": ".:53 {\n    errors\n    health {\n       lameduck 5s\n    }\n    ready\n    kubernetes cluster.local in-addr.arpa ip6.arpa {\n       pods insecure\n       fallthrough in-addr.arpa ip6.arpa\n       ttl 30\n    }\n    prometheus :9153\n    forward . 8.8.8.8\n    cache 30\n    loop\n    reload\n    loadbalance\n}\n"}}'
+    docker exec $k8s_master kubectl -n kube-system patch configmap/coredns -p '{"data":{"Corefile": ".:53 {\n    errors\n    health {\n       lameduck 5s\n    }\n    ready\n    kubernetes cluster.local in-addr.arpa ip6.arpa {\n       pods insecure\n       fallthrough in-addr.arpa ip6.arpa\n       ttl 30\n    }\n    prometheus :9153\n    forward . 8.8.8.8\n    cache 30\n    loop\n    reload\n    loadbalance\n}\n"}}'
   fi
 
-  flannel_config k8s-master
+  flannel_config $k8s_master
 
-  retry_run 20 2 "k8s_node_ready k8s-master"
+  retry_run 20 2 "k8s_node_ready $k8s_master $k8s_master"
 
   #
   # Deploy the K8s worker nodes (k8s-worker-<num>)
   #
 
   declare -a k8s_worker
+  local name
 
   for (( i=0; i<$num_workers; i++ )); do
-    k8s_worker[$i]=$(docker_run --network=$net --rm --name="k8s-worker-$i" --hostname="k8s-worker-$i" nestybox/ubuntu-bionic-k8s:latest)
+    worker_name=${cluster_name}-worker-${i}
+    k8s_worker[$i]=$(docker_run --network=$net --rm --name=$worker_name --hostname=$worker_name nestybox/ubuntu-bionic-k8s:latest)
     wait_for_inner_dockerd ${k8s_worker[$i]}
   done
 
@@ -271,20 +278,27 @@ function k8s_cluster_setup() {
   done
 
   for (( i=0; i<$num_workers; i++ )); do
-    retry_run 40 1 "k8s_node_ready k8s-master k8s-worker-$i"
+    worker_name=${cluster_name}-worker-${i}
+    retry_run 40 2 "k8s_node_ready $k8s_master $worker_name"
   done
 
   echo $kubeadm_join
 }
 
 # Tears-down a k8s cluster created with k8s_cluster_setup().
+#
+# usage: k8s_cluster_teardown cluster_name num_workers
 function k8s_cluster_teardown() {
-  local num_workers=$1
+  local cluster_name=$1
+  local num_workers=$2
+
+  local k8s_master=${cluster_name}-master
+  local worker_name
 
   for i in `seq 0 $(( $num_workers - 1 ))`; do
-    local worker=k8s-worker-$i
-    docker_stop $worker
+    worker_name=${cluster_name}-worker-${i}
+    docker_stop $worker_name
   done
 
-  docker_stop k8s-master
+  docker_stop $k8s_master
 }
