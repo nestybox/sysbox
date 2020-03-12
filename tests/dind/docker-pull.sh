2dc91e3dcbb4d6f431c8cc38eb243ff5892e40d7
ctalledo <ctalledo@nestybox.com>
2020-03-11 23:11:45 -0700

    Added docker-store volume manager tests and improved dind build tests.

--- tests/dind/docker-pull.sh
+++ tests/dind/docker-pull.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+# Retry a command $1 times until it succeeds. Wait $2 seconds between retries.
+# (borrowed from OCI runc/tests/integration/helpers.bash)
+function retry() {
+  local attempts=$1
+  shift
+  local delay=$1
+  shift
+  local i
+
+  for ((i = 0; i < attempts; i++)); do
+    $@ > /dev/null 2>&1
+    if [ "$?" -eq 0 ]; then
+	return 0
+    fi
+    sleep $delay
+  done
+
+  echo "Command \"$@\" failed $attempts times. Output: $?"
+  false
+}
+
 # dockerd start
 dockerd > /var/log/dockerd.log 2>&1 &
-sleep 2
+retry 10 1 "docker ps"
 
 # pull inner images
 docker pull busybox:latest
