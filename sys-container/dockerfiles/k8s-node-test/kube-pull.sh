b9e644b82db5c1f587644bfce139f8676449ea01
Cesar Talledo <ctalledo@nestybox.com>
2020-05-25 23:24:14 -0700

    Removed Docker from k8s-node-test image; created k8s-node-with-docker-test image.

--- sys-container/dockerfiles/k8s-node-test/kube-pull.sh
+++ sys-container/dockerfiles/k8s-node-test/kube-pull.sh
@@ -19,15 +19,8 @@ fi
 k8s_version=$1
 
 # dockerd start
-dockerd > /var/log/dockerd.log 2>&1 &
-dockerd_pid=$!
+containerd > /var/log/containerd.log 2>&1 &
 sleep 2
 
 # pull inner images
 kubeadm config images pull --kubernetes-version=$k8s_version
-docker pull quay.io/coreos/flannel:v0.11.0-amd64
-
-# dockerd cleanup (remove the .pid file as otherwise it prevents
-# dockerd from launching correctly inside sys container)
-kill $dockerd_pid
-rm -f /var/run/docker.pid
