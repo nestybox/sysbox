095427a1d54c4c48232914b65d617e95982347c7
Rodny Molina <rmolina@nestybox.com>
2020-06-22 18:22:12 -0400

    Extend installer to cover scenarios where docker's 'default-runtime' knob is utilized

--- tests/helpers/docker.bash
+++ tests/helpers/docker.bash
@@ -8,6 +8,10 @@
 
 load ../helpers/run
 
+function wait_for_dockerd {
+  retry_run 10 1 "docker ps"
+}
+
 function wait_for_inner_dockerd {
   local syscont=$1
   retry_run 10 1 "__docker exec $syscont docker ps"
