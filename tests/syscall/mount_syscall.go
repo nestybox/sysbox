f779ea8aca314752158858497bfc6b6ed0618a27
Rodny Molina <rmolina@nestybox.com>
2020-12-17 02:02:43 -0500

    Minor improvements [skip ci]
    Signed-off-by: Rodny Molina <rmolina@nestybox.com>

--- tests/syscall/mount_syscall.go
+++ tests/syscall/mount_syscall.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"golang.org/x/sys/unix"
 )
 
-func do_bind_mount(s, t string) {
+func do_bind_mount(s, t string) error {
 
 	err := unix.Mount(s, t, "", unix.MS_BIND, "")
 	if err != nil {
-		fmt.Printf("bind-mount error received %v:", err)
+		fmt.Printf("bind-mount error received: %v\n", err)
+		return err
 	}
+
+	return nil
 }
 
-func do_remount_ro(s, t string) {
+func do_remount_ro(s, t string) error {
 
 	err := unix.Mount(s, t, "", unix.MS_BIND|unix.MS_REMOUNT|unix.MS_RDONLY, "")
 	if err != nil {
-		fmt.Printf("RO remount error received %v:", err)
+		fmt.Printf("RO remount error received: %v\n", err)
+		return err
 	}
+
+	return nil
 }
 
-func do_remount_rw(s, t string) {
+func do_remount_rw(s, t string) error {
 
 	err := unix.Mount(s, t, "", unix.MS_BIND|unix.MS_REMOUNT, "")
 	if err != nil {
-		fmt.Printf("RW remount error received: %v", err)
+		fmt.Printf("RW remount error received: %v\n", err)
+		return err
 	}
+
+	return nil
 }
 
 func usage() {
@@ -40,18 +48,25 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) != 3 {
-		fmt.Printf("\nNumber of attributes received: %d, expected: %d\n", len(args), 3)
+		fmt.Printf("\nNumber of arguments received: %d, expected: %d\n", len(args), 3)
 		usage()
 		os.Exit(1)
 	}
 
+	var err error
+
 	if args[0] == "bind" {
-		do_bind_mount(args[1], args[2])
+		err = do_bind_mount(args[1], args[2])
 	} else if args[0] == "ro-remount" {
-		do_remount_ro(args[1], args[2])
+		err = do_remount_ro(args[1], args[2])
 	} else if args[0] == "rw-remount" {
-		do_remount_rw(args[1], args[2])
+		err = do_remount_rw(args[1], args[2])
 	} else {
-		log.Printf("Unsupported option: %s", args[0])
+		fmt.Printf("Unsupported command option: %s\n", args[0])
+		err = fmt.Errorf("Unsupported command option: %s", args[0])
+	}
+
+	if err != nil {
+		os.Exit(1)
 	}
 }
