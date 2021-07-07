c6e4e4081a7fea6efe658dcd002e7d9aa47673ca
Rodny Molina <rmolina@nestybox.com>
2021-07-07 00:53:29 -0400

    Skip K3s testing in scenarios with SElinux
    This is typically the case in Fedora distro where SElinux is enabled by default. In this case, we will skip K3s testsuite execution as it's still considered an experimental feature.
    
    Signed-off-by: Rodny Molina <rmolina@nestybox.com>

--- tests/kind/kind.sh
+++ tests/kind/kind.sh
@@ -4,29 +4,49 @@
 # support.
 #
 
-progName=$(basename "$0")
+# Finds out if selinux is enabled by looking at the file-system extended attributes.
+function selinux_on() {
+	if ls -l /proc/uptime | cut -d" " -f1 | tail -c 2 | egrep -q "\."; then
+		return 0
+	else
+		return 1
+	fi
+}
 
-# Argument testName is optional
-if [ $# -eq 1 ]; then
-  printf "\nExecuting $1 ... \n"
-  bats --tap $1
-  exit 0
-fi
+function main() {
 
-# the kind tests need plenty storage (otherwise kubelet fails);
-# remove all docker images from prior tests to make room, and
-# remove all docker images after test too.
-printf "\n"
-docker system prune -a -f
+	progName=$(basename "$0")
 
-printf "\nExecuting kind testcases with flannel cni ... \n"
-bats --tap tests/kind/kind-flannel.bats
+	# Argument testName is optional.
+	if [ $# -eq 1 ]; then
+		printf "\nExecuting $1 ... \n"
+		bats --tap $1
+		exit 0
+	fi
 
-printf "\nExecuting kind testcases with custom docker networks ... \n"
-bats --tap tests/kind/kind-custom-net.bats
+	# the kind tests need plenty storage (otherwise kubelet fails);
+	# remove all docker images from prior tests to make room, and
+	# remove all docker images after test too.
+	printf "\n"
+	docker system prune -a -f
 
-printf "\nExecuting k3s testcases with flannel cni ... \n"
-bats --tap tests/kind/k3s-flannel.bats
+	printf "\nExecuting kind testcases with flannel cni ... \n"
+	bats --tap tests/kind/kind-flannel.bats
 
-printf "\n"
-docker system prune -a -f
+	printf "\nExecuting kind testcases with custom docker networks ... \n"
+	bats --tap tests/kind/kind-custom-net.bats
+
+	# Skip k3s testcases if se-linux feature is activated (currently in experimental
+	# state by K3s).
+	if selinux_on; then
+		printf "\nSkipping k3s testcases due to SElinux activation ... \n"
+	else
+		printf "\nExecuting k3s testcases with flannel cni ... \n"
+		bats --tap tests/kind/k3s-flannel.bats
+	fi
+
+	printf "\n"
+	docker system prune -a -f
+}
+
+main "$@"
