d9581b454911678ab42268d45131b269025c8328
Cesar Talledo <ctalledo@nestybox.com>
2022-01-29 04:18:19 +0000

    Add test helpers to check if host supports shiftfs and/or idmapped-mounts.
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- tests/helpers/uid-shift.bash
+++ tests/helpers/uid-shift.bash
@@ -6,10 +6,47 @@ load ../helpers/run
 # Uid shifting helper functions
 #
 
-function host_supports_uid_shifting() {
-	# Note: for now, host support for uid-shifting implies the presence of the
-	# "shiftfs" module on the host's kernel. In the future, once support for
-	# ID-mapped mounts is added in Sysbox, uid-shifting will alternatively mean
-	# that the host kernel supports ID-mapped mounts.
-	modprobe shiftfs;
+function kernel_supports_shiftfs() {
+	modprobe shiftfs >/dev/null 2>&1 && lsmod | grep shiftfs
+}
+
+function kernel_supports_idmapped_mnt() {
+
+	local kernel_rel=$(uname -r)
+	local rel_major=$(echo ${kernel_rel} | cut -d'.' -f1)
+	local rel_minor=$(echo ${kernel_rel} | cut -d'.' -f2)
+
+	[ ${rel_major} -gt 5 ] || ( [ ${rel_major} -eq 5 ] && [ ${rel_minor} -ge 12 ] )
+}
+
+function sysbox_idmapped_mnt_disabled {
+	ps -fu | grep "$(pidof sysbox-mgr)" | grep -q "disable-idmapped-mount"
+}
+
+function sysbox_idmapped_mnt_enabled {
+	ps -fu | grep "$(pidof sysbox-mgr)" | grep -qv "disable-idmapped-mount"
+}
+
+function sysbox_using_idmapped_mnt() {
+	if kernel_supports_idmapped_mnt && sysbox_idmapped_mnt_enabled; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function sysbox_using_shiftfs() {
+	if kernel_supports_shiftfs && sysbox_idmapped_mnt_disabled; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function sysbox_using_uid_shifting() {
+	if sysbox_using_idmapped_mnt || sysbox_using_shiftfs; then
+		return 0
+	else
+		return 1
+	fi
 }
